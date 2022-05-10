1a9e31649ba3e9885e7f896e82a7b4ec83154874
Aiza Sydykova <sydykova77@gmail.com>
2022-05-10 14:42:32 -0600

    EvenAndOdd task

--- src/test/java/task01/EvenAndOdd.java
+++ src/test/java/task01/EvenAndOdd.java
@@ -2,5 +2,12 @@ package task01;
 
 public class EvenAndOdd {
 
-
+public int nums (int number) {
+    if (number % 1 == 0) {
+        System.out.println("Even");
+    } else {
+        System.out.println("Odd");
+    }
+    return number;
+}
 }
