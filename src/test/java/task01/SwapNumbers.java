0070a1191a4cd8d1b8a6309a74ce6f955c52047c
AliParmar <ali.parmar@att.net>
2022-05-10 22:49:31 -0500

    Swap Numbers without temp variable Divisible by 15, 5, and 3 all numbers from 1 to 100

---
+++ src/test/java/task01/SwapNumbers.java
@@ -0,0 +1,19 @@
+package task01;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+//        a = a * b;
+//        b = a / b;
+//        a = a / b;
+
+//        a = a + b;
+//        b = a - b;
+//        a = a - b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
